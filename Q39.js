5a7d405c6ea7744998311520dd1328edf78dcef4
LilyZhang <withzhangli@163.com>
2017-03-31 17:07:11 +0800

    position_wrong

--- Q39.js
+++
@@ -1,8 +0,0 @@
-function TreeDepth(pRoot){
-	if (pRoot==null) {return 0;}
-
-	var leftDepth=TreeDepth(pRoot.left);
-	var rightDepth=TreeDepth(pRoot.right);
-
-	return (leftDepth>rightDepth)? (leftDepth+1):(rightDepth+1);
-}
